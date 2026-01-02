82686cb68367d31d5c8a44b8964eece91d497c31
abdul-codes <abdulsalamkinsanya2@gmai.com>
2026-01-02 01:03:34 +0100

    Feat: Update API url

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { FoodRecommendationResponse, RecommendationRequest } from "./types";
 
-const API_URL = "http://localhost:8000"; // The address of our Python backend
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
 
 export async function getFoodRecommendations(
   request: RecommendationRequest,
